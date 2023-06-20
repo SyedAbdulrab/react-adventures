0dd6382d943dd598b48e4bde733e6cc7fc9ff8f6
SyedAbdulrab <syedabdulrab1133@gmail.com>
2023-06-21 01:50:09 +0500

    Add footer

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "../HeroSection";
 import Cards from "../Cards";
+import Footer from "../Footer";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <Cards />
+      <Footer/>
     </>
   );
 };

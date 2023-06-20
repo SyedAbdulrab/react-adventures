15702e4295a8c7809b1164a254b04afd6ad66aa5
SyedAbdulrab <syedabdulrab1133@gmail.com>
2023-06-21 00:56:58 +0500

    Add Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Fragment } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './components/pages/Home'
 function App() {
 
   return (
    <Fragment>
+    <Navbar/>
     <Routes>
-      <Route path='/' element={<Navbar/>}/>
+      <Route path='/' element={<Home/>}/>
     </Routes>
    </Fragment>
    
